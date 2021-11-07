03d7f088d867f0b1cc2822188e194e236d2f2cad
KMyroslav <kukhtarukm@gmail.com>
2021-11-08 00:56:59 +0200

    Updates styles

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -19,32 +19,37 @@ function ContactForm({ addContact }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor={nameInputId}>Name</label>
-      <input
-        id={nameInputId}
-        type="text"
-        name="name"
-        value={name}
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Casnumbermore d'Artagnan и т. п."
-        required
-        onChange={(e) => {
-          setName(e.currentTarget.value);
-        }}
-      />
-      <label htmlFor={numberInputId}>Number</label>
-      <input
-        id={numberInputId}
-        type="tel"
-        name="number"
-        value={number}
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-        required
-        onChange={(e) => {
-          setNumber(e.currentTarget.value);
-        }}
-      />
+      <div className="input-wrapper">
+        <label htmlFor={nameInputId}>Name</label>
+        <input
+          id={nameInputId}
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Casnumbermore d'Artagnan и т. п."
+          required
+          onChange={(e) => {
+            setName(e.currentTarget.value);
+          }}
+        />
+      </div>
+      <div className="input-wrapper">
+        {" "}
+        <label htmlFor={numberInputId}>Number</label>
+        <input
+          id={numberInputId}
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+          onChange={(e) => {
+            setNumber(e.currentTarget.value);
+          }}
+        />
+      </div>
       <button type="submit">Add contact</button>
     </form>
   );
